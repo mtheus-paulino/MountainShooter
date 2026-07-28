0a51db3f4334db87e50d0e570dd1d04dbe108d3a
Matheus Paulino <paulino.matheus@outlook.com.br>
2026-07-27 22:45:37 -0300

    Atualização para continuar depois

---
+++ code/game.py
@@ -0,0 +1,17 @@
+from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.menu import Menu
+
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+
---
+++ code/menu.py
@@ -0,0 +1,31 @@
+import Font
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+
+
+class Menu():
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png')    #carregar imagem de background
+        self.rect = self.surf.get_rect(left=0, top=0 ) #criando um retangulo
+
+    def run(self):
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)  # Com menos 1 toca pra sempre
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)  # aqui vamos desenhar
+            pygame.display.flip()
+
+            # Chek for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit() #Close Window
+                    quit() #end_pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)

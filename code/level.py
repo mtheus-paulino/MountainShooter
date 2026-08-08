f44e5ca61074dfc2c7f90086ea7af3d0e329cd3f
Matheus Paulino <paulino.matheus@outlook.com.br>
2026-08-08 16:21:14 -0300

    Fixing imports in leve.py file

--- code/level.py
+++ code/level.py
@@ -1,7 +1,13 @@
+import sys
 import pygame
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
 
+from code.const import C_WHITE, WIN_HEIGHT
 from code.entityFactory import EntityFactory
 from code.entity import Entity
+
 
 class Level:
     def __init__(self, window, name, game_mode):
@@ -10,11 +16,31 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000  # 20s
 
     def run(self, ):
+        pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', C_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps() :.0f}', C_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', C_WHITE, (10, WIN_HEIGHT - 20))
+
             pygame.display.flip()
         pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typerwriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
--- code/menu.py
+++ code/menu.py
@@ -9,7 +9,7 @@ from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
 class Menu():
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')    #carregar imagem de background
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()    #carregar imagem de background
         self.rect = self.surf.get_rect(left=0, top=0 ) #criando um retangulo
 
     def run(self):
